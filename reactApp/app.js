c1206e9012897cd7624bc7c0f63c70c27712a104
avramesh17 <avramesh17@gmail.com>
2018-02-16 16:42:25 -0800

    me

---
+++ reactApp/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*
+var React = require('react');
+var ReactDOM = require('react-dom');
+**/
+
+ReactDOM.render(<p>React lives!</p>,
+   document.getElementById('root'));
